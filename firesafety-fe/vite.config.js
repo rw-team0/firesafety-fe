f895616c3666e4cb1863e272a4795028c598e61b
soyoung <ssssy93@icloud.com>
2026-07-24 00:02:40 +0900

    test: 로컬 백엔드 연동 설정

--- firesafety-fe/vite.config.js
+++ firesafety-fe/vite.config.js
@@ -20,12 +20,13 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        //target: 'http://localhost:8080', // 실제 백엔드 주소로 변경
-        target: 'http://192.168.0.31:8080', // 실제 백엔드 주소로 변경
+        target: 'http://localhost:8080', // 실제 백엔드 주소로 변경
+        //target: 'http://192.168.0.31:8080', // 실제 백엔드 주소로 변경
         changeOrigin: true,
       },
       '/ws': {
-        target: 'ws://192.168.0.31:8080',
+        target: 'http://localhost:8080',
+        //target: 'ws://192.168.0.31:8080',
         ws: true,
         changeOrigin: true,
       },
